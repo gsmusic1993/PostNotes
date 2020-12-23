7f520e8eadb74701c0b141bb562b1184a9c38bd3
gsmusic1993 <gsmusic1993@gmail.com>
2020-12-23 09:15:54 +0530

    NotesChanges

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,93 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="paddCss">
+    <b-button variant="outline-primary" @click="onAddNoteReq">
+      Add Note
+    </b-button>
+    <br />
+
+    <AddNote
+      v-for="x in notes"
+      :noteObj="x"
+      @updateNote="updateNotefn"
+      :key="x.id"
+    ></AddNote>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AddNote from "./components/AddNote";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      notes: [{ note: "Note 1", id: "0", postion: "fixed", left: "500px", top: "200px",
+      color : "#DFECFE" }],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    AddNote,
+  },
+  methods: {
+    onAddNoteReq: function () {
+      let len = Number(this.notes.length);
+      len = ++len;
+      let newStr = "Note " + len;
+      var leftX = String(parseInt(Math.random()*100) * 2).concat("px");
+      var leftY = String(parseInt(Math.random()*100)* 5).concat("px");
+      console.log(leftX+","+leftY);
+      let sColor = this.getRandomColor();
+      this.notes.push({ id: String(len), note: newStr, postion: "fixed", left: leftX, top: leftY, color: sColor });
+      localStorage.setItem("notes", JSON.stringify(this.notes));
+    },
+
+     getRandomColor: function() {
+                var letters = 'BCDEF'.split('');
+                var color = '#';
+                for (var i = 0; i < 6; i++ ) {
+                    color += letters[Math.floor(Math.random() * letters.length)];
+                }
+                return color;
+            },
+
+    updateNotefn: function (Obj) {
+    
+      var sIndex = "";
+      for (var i = 0; i < this.notes.length; i++) {
+        if (this.notes[i].id === Obj.id) {
+          sIndex = i;
+          break;
+        }
+      }
+      if (sIndex !== "") {
+        // this.notes[index].note = newValue;
+        this.notes = this.notes.map((item, index) => {
+          if (index !== sIndex) {
+           
+            return item;
+          }          
+          return {
+            ...item,
+            ...Obj,
+          };
+        });
+        localStorage.setItem("notes", JSON.stringify(this.notes));
+      }
+    },
+
+  },
+  beforeMount() {
+    console.log("Before Mount Call");
+    if (JSON.parse(localStorage.getItem("notes")) !== null)
+      this.notes = JSON.parse(localStorage.getItem("notes"));
+    console.log(JSON.parse(localStorage.getItem("notes")));
+  },
+  updated() {
+    console.log("updated Call");
+    console.log(this.notes);
+  },
+  computed: {},
+};
 </script>
 
 <style>
@@ -24,5 +98,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.paddCss
+{
+  padding : 1rem;
 }
 </style>
---
+++ src/components/AddNote.vue
@@ -0,0 +1,74 @@
+<template>
+  <div
+    class="noteBox"
+    draggable="true"
+    @dragstart="drag_start"
+    @dragend="drop"
+    :style="{ position: noteObj.postion, left: noteObj.left, top: noteObj.top }"
+  >
+
+    <b-form-textarea
+      :value="noteObj.note"
+      :name="noteObj.id"
+      @change="onChangeVal"
+      class="textBox"
+      :style="{ background : noteObj.color }"
+      id="textarea"
+      placeholder="Enter Note Text"
+      rows="3"
+      max-rows="6"
+    >
+    </b-form-textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddNote",
+  props: {
+    noteObj: Object,
+  },
+
+  methods: {
+    onChangeVal: function (sNewVal) {
+
+      this.noteObj.note = sNewVal;
+      this.$emit("updateNote", this.noteObj );
+    },
+
+    drag_start: function (event) {
+      var style = window.getComputedStyle(event.target, null);
+      this.startX = parseInt(style.getPropertyValue("left"), 10) - event.clientX;
+      this.startY = parseInt(style.getPropertyValue("top"), 10) - event.clientY ;
+    },
+
+    drag_over: function (event) {
+      event.preventDefault();
+      return false;
+    },
+
+    drop: function (event) {
+      this.noteObj.left = event.clientX + parseInt(this.startX, 10) + "px";
+      this.noteObj.top = event.clientY + parseInt(this.startY, 10) + "px";
+      console.log(this.noteObj.left + "," + this.noteObj.top);
+      this.$emit("updateNote", this.noteObj );
+     
+      event.preventDefault();
+      return false;
+    },
+  },
+  
+};
+</script>
+
+<style>
+.noteBox {
+  width: 25%;
+  height: 50%;
+  border: 0.1rem solid black !important;
+}
+.textBox
+{
+  height: 100% !important;
+}
+</style>
